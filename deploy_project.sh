8e4a0caeba4c24947485d0badc9881ca1aa85000
Vladimir Savin <140637542+Borlandcsn@users.noreply.github.com>
2025-03-01 21:58:20 +0300

    Update deploy_project.sh

--- deploy_project.sh
+++ deploy_project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir opt
 cd opt
-git clone https://github.com/Hoaxlt/DevOps-study
-cd DevOps-study
+git clone https://github.com/Borlandcsn/shvirtd-example-python.git
+cd shvirtd-example-python
 docker compose up -d
